de056f44e06f5eef4b9ddb249e5ae5c4f42b59ba
Eric Young <ejyoung@usgs.gov>
2013-09-04 11:32:10 -0600

    Gained ability to download file

--- geostat.py
+++ geostat.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib.request
 import argparse
 import datetime
 
@@ -11,10 +11,23 @@ def setupEnv():
     
     ## Setup all runtime configurations here ##
     configs["observatory"] = args.observatory
-    configs["url"] = "http://magweb.cr.usgs.gov/data/magnetometer/"
+    configs["url"] = "http://magweb.cr.usgs.gov/data/magnetometer"
     return configs
     
 def start_http_session( url ):
-    
+    request = urllib.request.urlopen(url)
+    magdata = open("magdata.sec", "w")
+    #print ( request.read() )
+    magfile = request.read().decode("utf-8")
+    magfile.splitlines()
+    for line in magfile:
+        line = line.strip()
+        magdata.write(line+"\n")
+    #magdile = request.split()
+    #SSfor x in magfile
     
 runtimeConfigs = setupEnv()
+requestString = "{url}/{observatory}/{type}/{file}"
+
+#Make dynamic later
+start_http_session( requestString.format( url = runtimeConfigs["url"], observatory = runtimeConfigs["observatory"], type = "OneMinute", file="frd20130402vmin.min") )
