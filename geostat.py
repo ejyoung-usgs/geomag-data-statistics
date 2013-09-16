1d460cfb07968539c6a48ec9f6188648cc3b63d4
Eric Young <ejyoung@usgs.gov>
2013-09-16 11:47:03 -0600

    Add code to generate file name to pull

--- geostat.py
+++ geostat.py
@@ -27,10 +27,18 @@ def start_http_session( url ):
     #magdile = request.split()
     #SSfor x in magfile
     
+def form_file_name(obs_str, date):
+    file_template = "{obs}{year:4d}{month:02d}{day:02d}vmin.min"
+    today_year = date.year
+    today_month = date.month
+    today_day = date.day
+    
+    return file_template.format( obs = obs_str, year = today_year, month = today_month, day = today_day )
+    
 runtimeConfigs = setupEnv()
 requestString = "{url}/{observatory}/{type}/{file}"
 today_date = datetime.datetime.utcnow()
-today_date.resolution( datetime.timedelta(minutes=1) )
+
 ## Clear seconds ##
 t_delta = datetime.timedelta( seconds = today_date.second, microseconds= today_date.microsecond )
 today_date = today_date - t_delta
@@ -48,3 +56,5 @@ print( today_date - t_delta_1_hour )
 
 #Make dynamic later
 start_http_session( requestString.format( url = runtimeConfigs["url"], observatory = runtimeConfigs["observatory"], type = "OneMinute", file="frd20130402vmin.min") )
+
+print( form_file_name("FRD", today_date) )
