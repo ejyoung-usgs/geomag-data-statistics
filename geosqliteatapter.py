6372554a96c5a0141416c780369cb87bbad3be40
Eric Young <ejyoung@usgs.gov>
2013-10-31 11:41:32 -0600

    Fixed database to init tables for locations

--- geosqliteatapter.py
+++ geosqliteatapter.py
@@ -1,8 +1,11 @@
 import sqlite3
 
 class SqliteAdapter:
+
     def __init__(self, database):
         self.__db_connection = sqlite3.connect(database)
+        self.__delays = [0, 60, 300, 600, 900]
+        self.__locations = ["FRD", "BOU", "BDT"]
         self.init_database()
 
     def __del__(self):
@@ -10,9 +13,9 @@ class SqliteAdapter:
 
     def init_database(self):
         cursor = self.__db_connection.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS GeoStats ( _id INTEGER AUTO_INCREMENT , date TEXT, h INTEGER, d INTEGER, z INTEGER, f INTEGER, point_count INTEGER,observatory_fk INTEGER,  delay_fk ,PRIMARY KEY(_id), FOREIGN KEY(observatory_fk) REFERENCES Locations(_id), FOREIGN KEY(delay_fk) REFERENCES Delays(_id) )")
+        cursor.execute("CREATE TABLE IF NOT EXISTS GeoStats ( _id INTEGER , h INTEGER, d INTEGER, z INTEGER, f INTEGER, point_count INTEGER,observatory_fk INTEGER,  delay_fk ,PRIMARY KEY(_id), FOREIGN KEY(observatory_fk) REFERENCES Locations(_id), FOREIGN KEY(delay_fk) REFERENCES Delays(_id) )")
         cursor.execute("CREATE TABLE IF NOT EXISTS Locations( _id INTEGER, observatory_name TEXT, PRIMARY KEY(_id) )" )
-        cursor.execute("CREATE TABLE IF NOT EXISTS Delays ( _id INTEGER AUTO_INCREMENT, delay INTEGER, PRIMARY KEY(_id) ) ") #### Delay is in seconds ####
+        cursor.execute("CREATE TABLE IF NOT EXISTS Delays ( _id INTEGER, delay INTEGER, PRIMARY KEY(_id) ) ") #### Delay is in seconds ####
         self.__db_connection.commit();
         #### Setup Observatory locations ####
         cursor.execute("SELECT _id FROM Locations")
@@ -32,6 +35,15 @@ class SqliteAdapter:
             self.insert_delay(600)
             self.insert_delay(900)
 
+        for id in self.__locations:
+            check_stat_query = "select * from GeoStats where observatory_fk=? and delay_fk=?"
+            location_id = self.find_location_id_by_name(id)
+            for delay in self.__delays:
+                delay_id = self.find_delay_id_by_value(delay)
+                cursor.execute(check_stat_query, (location_id, delay_id,) )
+                if len(cursor.fetchall() ) == 0 :
+                    self.__insert_stat(location_id, delay_id)
+
     def insert_observatory(self, location):
         cursor = self.__db_connection.cursor()
         cursor.execute("INSERT INTO Locations (observatory_name) VALUES(?)", (location,) )
@@ -45,3 +57,19 @@ class SqliteAdapter:
     def select_stat(self, location, type, delay):
         cursor = self.__db_connection.cursor()
         cursor.execute("SELECT ?, point_count FROM GeoStats INNER JOIN Locations ON observatory_fk = Locations._id INNER JOIN delay_fk = Delays._id where Locations._id = ? and Delays._id = ?", (type, location, delay,))
+
+    def __insert_stat(self, location, delay):
+        cursor = self.__db_connection.cursor()
+        query = "insert into GeoStats(observatory_fk, delay_fk, h, d, z, f, point_count) values (?, ? , 0, 0, 0, 0, 0)"
+        cursor.execute( query, (location, delay,) )
+
+    def find_location_id_by_name(self, name):
+        cursor = self.__db_connection.cursor()
+        query = "select _id from Locations where observatory_name = ?"
+        cursor.execute(query, (name,))
+        return cursor.fetchall()[0][0]
+    def find_delay_id_by_value(self, delay):
+        cursor = self.__db_connection.cursor()
+        query = "select _id from Delays where delay=?"
+        cursor.execute(query, (delay,) )
+        return cursor.fetchall()[0][0]
